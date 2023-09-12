c78489bf5f6d9b0898b220e734591414aeacc714
AyhaamB <ayh132@gmail.com>
2023-09-11 22:41:04 -0400

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/route1.js');
 
-const PORT = 3001;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -23,6 +23,6 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
